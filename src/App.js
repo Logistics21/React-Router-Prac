6d48d0ce594efa91050898a85dfdbfc98670f554
David Halsey <dhalsey212@gmail.com>
2017-09-22 18:28:22 -0400

    Test NavLink styling

--- src/App.js
+++ src/App.js
@@ -2,23 +2,11 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Link
 } from 'react-router-dom';
 
 import './App.css';
 
-// An Object can also be interlopated into the Link Component
-// the to prop becomes pathname key
-// replace controls browser history -- replaces the previous URL with the
-// newest URL
-const Links = () => (
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to={{pathname: '/about'}}>About</Link>
-    <Link replace to="/contact">Contact</Link>
-  </nav>
-)
-
+// URL parameters are a methods by which we can extract parameters from our URL
 const App = () => (
   <Router>
     <div>
@@ -32,6 +20,60 @@ const App = () => (
 
 export default App;
 
+
+// const App = () => (
+//   <Router>
+//     <div>
+//       <Links />
+//       <Route exact path="/" render={() => <h1>Home</h1>} />
+//       <Route path="/about" render={() => <h1>About</h1>} />
+//       <Route path="/contact" render={() => <h1>Contact</h1>} />
+//     </div>
+//   </Router>
+// )
+//
+// export default App;
+
+
+// NavLinks identifies the current route and gives it an an activeClassName
+// Unlike link NavLink is aware of a route, it qualifies the route
+// the same way the Route Component does
+// To match route Component simply add any qualifies from that route to the NavLink
+// activeStyle allows inline styling
+// isActive prop
+// isActiveFunc fires each time a navlink is rendered
+// every time the route is change the isActive prop is checked and the
+// isActiveFunc is fired -- the match data will be null unless the
+
+// const isActiveFunc = (match, location) => {
+//   console.log(match, location);
+//   return match;
+// }
+//
+// const Links = () => (
+//   <nav>
+//     <NavLink exact activeClassName="active" to="/">Home</NavLink>
+//     <NavLink activeStyle={{color: "green"}} to={{pathname: '/about'}}>About</NavLink>
+//     <NavLink
+//       isActive={isActiveFunc}
+//       activeClassName="active"
+//       to="/contact">Contact</NavLink>
+//   </nav>
+// )
+
+
+// An Object can also be interlopated into the Link Component
+// the to prop becomes pathname key
+// replace controls browser history -- replaces the previous URL with the
+// newest URL
+
+// const Links = () => (
+//   <nav>
+//     <Link to="/">Home</Link>
+//     <Link to={{pathname: '/about'}}>About</Link>
+//     <Link replace to="/contact">Contact</Link>
+//   </nav>
+// )
 
 
 // const Home = (props) => {
